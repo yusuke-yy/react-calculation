67c579e5a6baee36f80d45b94272fc14a6cc0531
yusuke-yy <52813072+yusuke-yy@users.noreply.github.com>
2021-07-09 14:36:26 +0900

    JSXで構造を作る #4

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="数字を入力" />
+        ×
+        <input placeholder="数字を入力" />
+        <button>計算</button>
+      </div>
+      <div>
+        <p>結果</p>
+      </div>
+    </>
+  );
 };
